b37952b970390b4edf178a9805c5c1984736afa6
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-07-05 22:31:05 +0200

    BackPageBtn created

--- src/components/CloseBtn/CloseBtn.jsx
+++ src/components/CloseBtn/CloseBtn.jsx
@@ -9,8 +9,8 @@ export const CloseBtn = ()=>{
     return(
         <>
         <button onClick={()=> navigate(-1)} className="Close-btn Small-btn" title='Volver'>
-            <svg className='Close-svg Small-btn--svg' width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g id="Menu / Close_LG">
+            <svg className='Close-svg Small-btn--svg' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g>
                 <path id="Vector" d="M21 21L12 12M12 12L3 3M12 12L21.0001 3M12 12L3 21.0001" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </g>
             </svg>
--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -4,6 +4,7 @@ import './Movie.css'
 import { PlayMovieBtn } from '@components/PlayMovieBtn/PlayMovieBtn'
 import { FavBtn } from '@components/FavBtn/FavBtn'
 import { MainInfo } from '@components/MainInfo/MainInfo'
+import { BackBtn } from '@components/BackBtn/BackBtn'
 import { KudiContext } from '@context/Context'
 
 import { useNavigate, useParams } from 'react-router-dom'
@@ -83,7 +84,8 @@ export const Movie = ()=>{
                 <svg className='Chevron-svg' onClick={scrollToInfo} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67"/>
                 </svg>
-            </div>    
+            </div>
+            <BackBtn/>    
         </article>
         <MainInfo/>
         </>
