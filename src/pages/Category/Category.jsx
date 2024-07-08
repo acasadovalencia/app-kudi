abddaf8a16b4c261825f13c0f60b57cf569688a4
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-07-08 22:11:53 +0200

    Update Category.jsx

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -13,7 +13,7 @@
 
         const { VITE_API , user , setUser , users , setCurrentUser , movies , tvshows } = useContext( KudiContext )
 
-        const { category } = useParams
+        const { category } = useParams()
 
 
         return(
