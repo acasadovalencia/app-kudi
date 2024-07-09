10eb59d8ab20b1a8964429734708f61e9a10df74
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-07-09 18:17:34 +0200

    Categories filtering.
    Reorganizing code

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,9 @@ import { BrowserRouter , Routes , Route} from 'react-router-dom'
 function App() {
   // States
   const [ login , setLogin] = useState()
+  const [ movies , setMovies] = useState([])
   const [ movie , setMovie] = useState([])
+  const [ tvshows , setTvshows] = useState([])
   const [ tvshow , setTvshow] = useState([])
   const [ users , setUsers] = useState([])
   const [ user , setUser] = useState([])
@@ -35,8 +37,36 @@ function App() {
           
   }
 
+  const getMovies = async ()=>{
+    let controller = new AbortController()
+    let options = {
+        method : 'get',                         // Método GET porque se piden datos
+        signal: controller.signal
+    }
+    await fetch(`${VITE_API}/movies` , options) // Fetch al endpoint /movie para obtener los datos
+    .then(res => res.json())
+    .then( data => setMovies(data))             // Setear datos de la respuesta a movies
+    .catch( err => console.log(err.message))    // Capturar y mostrar error
+    .finally(()=> controller.abort())           // Abortar conexión con API
+
+  }
+
+  const getTvshows = async ()=>{
+    let controller = new AbortController()
+    let options = {
+        method : 'get',                         // Método GET porque se piden datos
+        signal: controller.signal
+    }
+    await fetch(`${VITE_API}/tvshows` , options) // Fetch al endpoint /movie para obtener los datos
+    .then(res => res.json())
+    .then( data => setTvshows(data))             // Setear datos de la respuesta a movies
+    .catch( err => console.log(err.message))    // Capturar y mostrar error
+    .finally(()=> controller.abort())           // Abortar conexión con API
+
+}
+
   return (
-    <KudiContext.Provider value={{ VITE_API , login , setLogin , movie , setMovie , users , setUsers , getUsers , user , setUser , currentUser , setCurrentUser , tvshow , setTvshow }}>          {/* // Contexto para compartir con el resto de la APP */}
+    <KudiContext.Provider value={{ VITE_API , login , setLogin , movies , setMovies , getMovies , movie , setMovie , users , setUsers , getUsers , user , setUser , currentUser , setCurrentUser , tvshow , setTvshow , getTvshows , tvshows , setTvshows }}>          {/* // Contexto para compartir con el resto de la APP */}
     <BrowserRouter>                                                                 {/* // Creacion de rutas para navegar por diferentes paginas */}
     <>
     <Routes>
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -7,22 +7,44 @@
 
     import { useContext , useState , useEffect } from 'react'
     import { useNavigate , NavLink, useParams } from 'react-router-dom'
+import { Movies } from '../Movies/Movies'
+import { MoviesLi } from '../../components/MoviesLi/MoviesLi'
 
 
     export const Category = ()=>{
 
-        const { VITE_API , user , setUser , users , setCurrentUser , movies , tvshows } = useContext( KudiContext )
+        const { VITE_API , user , setUser , users , setCurrentUser , movies , getMovies , tvshows , setMovies } = useContext( KudiContext )
+
+        const [ moviesFiltered , setMoviesFiltered] = useState([])
 
         const { category } = useParams()
+
+        //Effects
+        useEffect(()=>{                                         
+            getMovies()                                     // Obtener las peliculas en un effect sin depedenccias para guardarlas cuando se cree el componente
+        }, [])
+
+        useEffect(()=>{
+            getMoviesFiltered()                             // Obtener el array de peliculas filtradas en un effect que se ejecutará cuando movies cambie, esperando a ejecutarse a que movies tenga contenido
+            console.log(moviesFiltered)
+        }, [movies])
+
+
+        const getMoviesFiltered = ()=>{
+            const filter = movies && movies.filter(eachMovie => eachMovie.genre.includes(category) )
+            setMoviesFiltered(filter)
+        }
 
 
         return(
             <>
             <main className="Main">
-            <section className="Section Categories">
-                <h2 className="Section-h2 H2">Categoría {category}</h2>
-                <ul className="Section-ul Categories-ul">
-                    
+            <section className="Section Category">
+                <h2 className="Section-h2 H2">{category}</h2>
+                <ul className="Section-ul Category-ul">
+                    {moviesFiltered.length != 0 && moviesFiltered.map( eachMovie => 
+                        <MoviesLi key={eachMovie._id} {...eachMovie}/>
+                    )}
                 </ul>
             </section>
             </main>
