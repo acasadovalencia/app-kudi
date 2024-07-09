abb68aed4216ce1ea5a40479d1fa0dc767facd20
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-07-09 20:58:53 +0200

    reorganize code

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,21 +1,17 @@
     import './Category.css'
 
     import { KudiContext } from '@context/Context'
-    import { PlayBtn } from '@components/PlayBtn/PlayBtn'
-    import { DeleteFavMovieBtn } from '@components/DeleteFavMovieBtn/DeleteFavMovieBtn'
-    import { DeleteFavTvshowBtn } from '@components/DeleteFavTvshowBtn/DeleteFavTvshowBtn'
+    import { MoviesLi } from '@components/MoviesLi/MoviesLi'
+    import { TvShowsLi } from '@components/TvShowsLi/TvShowsLi'
 
     import { useContext , useState , useEffect } from 'react'
-    import { useNavigate , NavLink, useParams } from 'react-router-dom'
-    import { Movies } from '../Movies/Movies'
-    import { MoviesLi } from '../../components/MoviesLi/MoviesLi'
-    import { TvShowsLi } from '../../components/TvShowsLi/TvShowsLi'
+    import { useParams } from 'react-router-dom'
 
 
 
     export const Category = ()=>{
 
-        const { VITE_API , user , setUser , users , setCurrentUser , movies , getMovies , tvshows , setMovies  , getTvshows , type , setType } = useContext( KudiContext )
+        const { movies , getMovies , tvshows , getTvshows } = useContext( KudiContext )
 
         const [ moviesFiltered , setMoviesFiltered] = useState([])
         const [ tvShowsFiltered , setTvShowsFiltered] = useState([])
