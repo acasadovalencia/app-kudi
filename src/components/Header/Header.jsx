f41d38b3ebbf3907ca92943a8725c9def281343e
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-07-13 19:02:38 +0200

    fixing submenu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -51,6 +51,13 @@ export const Header = ()=>{
                         <li className={`Nav-li ${currentPage.pathname == '/kudi/favlist' ? `CurrentPage` : ``}`}>
                         <NavLink to={`/kudi/favlist`}>Mi lista</NavLink>
                         </li>
+                        <li className="Nav-li">
+                        <button className="Search-btn">
+                            <svg className='Search-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                            </svg>
+                        </button>
+                        </li>
                     </ul>
                 </nav>
                 <div onClick={toggleMenu} className={`Header-profile Profile ${isMenuOpen ? `isOpen` : `` || modifyModal ? `isOpen` : ``}`}>
